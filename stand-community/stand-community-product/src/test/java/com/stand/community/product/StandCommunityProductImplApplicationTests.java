009f38fb3b8e204e375fbba1189415da6c1f0adc
Mrchen <2304617991@qq.com>
2022-07-10 20:54:27 +0800

    base

---
+++ stand-community/stand-community-product/src/test/java/com/stand/community/product/StandCommunityProductImplApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stand.community.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StandCommunityProductImplApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
